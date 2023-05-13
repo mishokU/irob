5101a540fd7b13430bc1ee1dd4211b8e6e729e0f
m.usov <m.usov@okko.tv>
2023-05-13 15:21:01 +0300

    Fix path

--- client/src/constants/Constants.ts
+++ client/src/constants/Constants.ts
@@ -1,4 +1,4 @@
 
 
-export const ServerUrl: string = "http://localhost:8012/api"
+export const ServerUrl: string = "http://51.250.66.59:8012/api"
 export const email: string = "irob.business@mail.ru"
